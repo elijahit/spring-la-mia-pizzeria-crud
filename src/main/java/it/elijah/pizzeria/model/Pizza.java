96e6ff45c535f3bfc024cc7f09560771f37d3992
Elijah <64620855+elijahit@users.noreply.github.com>
2024-07-01 16:03:00 +0200

    ADD: Model Pizza.java

---
+++ src/main/java/it/elijah/pizzeria/model/Pizza.java
@@ -0,0 +1,59 @@
+package it.elijah.pizzeria.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="pizze")
+public class Pizza {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false)
+	private String nome;
+	@Column(nullable = false)
+	private String descrizione;
+	
+	@Column(name="foto_url", nullable=false)
+	private String fotoUrl;
+	@Column(nullable = false)
+	private double prezzo;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescrizione() {
+		return descrizione;
+	}
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	public String getFotoUrl() {
+		return fotoUrl;
+	}
+	public void setFotoUrl(String fotoUrl) {
+		this.fotoUrl = fotoUrl;
+	}
+	public double getPrezzo() {
+		return prezzo;
+	}
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+	
+}
